9fa72b60e26a265e21ff30fd5fc176b6307eb1bd
centralhex24-ib <centralhex24@gmail.com>
2026-03-30 11:22:32 +0000

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,24 @@
+global.API_KEYS = {
+  
+  // News
+  NEWS_API: "5e879ed22b9445c180590559a46f0594",
+
+  // Remove Background
+  REMOVE_BG: "TA_CLE_REMOVE_BG",
+
+  // Crypto
+  COINGECKO: "TA_CLE_COINGECKO",
+
+  // Stock market
+  ALPHA_VANTAGE: "TA_CLE_ALPHA_VANTAGE",
+
+  // Football
+  FOOTBALL_API: "TA_CLE_FOOTBALL",
+
+  // Cricket
+  CRICKET_API: "TA_CLE_CRICKET",
+
+  // AI
+  OPENAI: "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"
+
+}
